30880217c7323bde4eabe425532bc613ec171696
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-03-12 12:17:28 +0100

    Added gitignore

--- RISCV32/tests/ripes/simple.s
+++
@@ -1,7 +0,0 @@
-.text
- main:
-
-li a0, 0
-li a7, 93
-ecall
-
--- RISCV32/tests/simple/shift.s
+++
@@ -1,8 +0,0 @@
-.text
-    lui x10 0xff000
-    addi x11 x0 255
-	srai x10 x10 8
-	slli x11 x11 8
-	addi x11 x11 255
-	add x12 x10 x11
-	addi x17 x0 10
