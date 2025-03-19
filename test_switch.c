2e5296b1620f34f3d67b925f154023aad74033ce
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-03-19 12:20:14 +0100

    New test

--- test_switch.c
+++ test_switch.c
@@ -2,7 +2,11 @@
 
 int main(void) {
     while(1) {
-        setLed(0, readSwitch(0));
+        if(readSwitch(0)) {
+            setLed(0, 1);
+        } else {
+            setLed(0, 0);
+        }
     }
     return 0;
 }
