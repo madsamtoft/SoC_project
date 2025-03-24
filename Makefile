2678ca11e7a7ccbb8c502d4e744df2c63abe1372
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-03-24 17:43:36 +0100

    Changed make functionality

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
-file = main
+in = main
+out = main
 
 make:
 	riscv32-unknown-elf-gcc $(file).c -o $(file).out
 nolib:
 	riscv32-unknown-elf-gcc $(file).c -o $(file).out -Os -nodefaultlibs -nostdlib
 nostart:
-	riscv32-unknown-elf-gcc $(file).c -o $(file).out -Os -nostartfiles
+	riscv32-unknown-elf-gcc $(file).c -o $(out).out -Os -nostartfiles
 small:
 	riscv32-unknown-elf-gcc $(file).c -o $(file).out -Os -nostartfiles -nodefaultlibs -nostdlib
 clean:
--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 sudo make clean
-make nostart file=tests/test_button
-make nostart file=tests/test_switch
-make nostart file=tests/test_switches
-make nostart file=tests/test_blink
-make nostart file=tests/test_blink_single
-make nostart file=tests/test_gamepad_uart
-make nostart file=tests/test_keyboard_uart
+make nostart file=tests/test_button out=out/test_button
+make nostart file=tests/test_switch out=out/test_switch
+make nostart file=tests/test_switches out=out/test_switches
+make nostart file=tests/test_blink out=out/test_blink
+make nostart file=tests/test_blink_single out=out/test_blink_single
+make nostart file=tests/test_usb_uart out=out/test_usb_uart
+make nostart file=tests/test_keyboard_uart out=out/test_keyboard_uart
