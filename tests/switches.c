1e48718e82a9caa05f7ccd4fe92ca3cb9bd110c8
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-04-09 17:29:04 +0200

    switches

--- tests/switches.c
+++ tests/switches.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 
 int main(void) {
-    int uart_ready = uartReady();
-    int led_state = getLeds();
+    int uart_ready = 0;
+    int led_state = 0;
     while (1) {
         for (int i = 0; i < SWITCH_COUNT; i++) {
             setLed(i, readSwitch(i));
-        }
-        uart_ready = uartReady();
-        led_state = getLeds();
-        if (uart_ready & 0b1) {
-            setUart(led_state);
-        }
-        delay(100000);
-        uart_ready = uartReady();
-        led_state = getLeds();
-        if (uart_ready & 0b1) {
-            setUart(led_state);
+
+            uart_ready = uartReady();
+            led_state = getLeds();
+            if (uart_ready & 0b1) {
+                setUart(led_state & 0xF);
+            }
+            delay(100000);
+            uart_ready = uartReady();
+            led_state = getLeds();
+            if (uart_ready & 0b1) {
+                setUart(led_state & 0xF);
+            }
         }
     }
     return 0;
