9b32226c5e7a45add0bfcdccbf92f309fbb239d7
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-04-09 17:07:09 +0200

    Tried adding uart tests

--- tests/switches.c
+++ tests/switches.c
@@ -1,11 +1,18 @@
 #include "wildio.h"
+#include <stdio.h>
 
 int main(void) {
     while (1) {
         for (int i = 0; i < SWITCH_COUNT; i++) {
             setLed(i, readSwitch(i));
         }
+        if (uartReady() & 0b1) {
+            setUart(led_state);
+        }
         delay(100000);
+        if (uartReady() & 0b1) {
+            setUart(led_state);
+        }
     }
     return 0;
 }
