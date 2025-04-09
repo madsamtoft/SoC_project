121d2ca75e3703b3e58599e2fe01db48849e85be
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-04-09 17:37:02 +0200

    l

--- tests/switches.c
+++ tests/switches.c
@@ -4,21 +4,17 @@
 int main(void) {
     int uart_ready = 0;
     int led_state = 0;
+    int switch_state = 0;
     while (1) {
         for (int i = 0; i < SWITCH_COUNT; i++) {
-            setLed(i, readSwitch(i));
-
-            uart_ready = uartReady();
-            led_state = getLeds();
-            if (uart_ready & 0b1) {
-                setUart(led_state & 0xF);
-            }
-            delay(100000);
-            uart_ready = uartReady();
-            led_state = getLeds();
-            if (uart_ready & 0b1) {
-                setUart(led_state & 0xF);
-            }
+            switch_state = readSwitch(i);
+            setLed(i, switch_state);
+        }
+        delay(100000);
+        uart_ready = uartReady();
+        led_state = getLeds();
+        if (uart_ready & 0b1) {
+            setUart(led_state & 0xF);
         }
     }
     return 0;
