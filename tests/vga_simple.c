b40056427d6b45f49240cc94ab0ae1e10c5ed31e
Dejan <deja0079@outlook.dk>
2025-04-25 08:02:23 +0200

    Working on VGA library

--- tests/vga_simple.c
+++ tests/vga_simple.c
@@ -1,15 +1,24 @@
-#include "wildio.h"
+// #include "wildio.h"
+// #include <stdio.h>
+
+#define VGA_BASE_ADDR 0xf0100000
+volatile char * const vga = (volatile char *) VGA_BASE_ADDR;
+
+
+void setPixel(int x, int y, char c);
+// void wait(int cc);
 
 int main(void) {
-    int xlim = 0b10100000;
-    int ylim = 0b01111000;
-    int clim = 0b111111;
+    int xlim = 320/4;
+    int ylim = 240;
+    char clim = 0b111111;
 
     int x = 0;
     int y = 0;
-    int c = 0;
+    char c = 0;
 
     while(1) {
+        setPixel(x, y, c);
         x = x+1;
         if (x >= xlim) {
             x = 0;
@@ -22,6 +31,20 @@ int main(void) {
                 }
             }
         }
-        setPixel(x, y, c);
     }
+    return 0;
+}
+
+// void wait(int cc) {
+//     volatile int i = 0;
+//     while(i < cc) {
+//         i += 1;
+//     }
+// }
+
+void setPixel(int x, int y, char c) {
+    volatile char * addrPtr = vga + (x & 0x1ff) + ((y & 0xff) << 9);
+    // printf("%x, %d\n", addrPtr, c);
+    *addrPtr = c;
+    return;
 }
