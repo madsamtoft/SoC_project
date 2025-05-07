2089ef6df504007600db05074b2953ad1f8b1f7e
Dejan <deja0079@outlook.dk>
2025-05-07 15:40:59 +0200

    Adjusted test.sh for windows

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ file = main
 mac:
 	riscv32-unknown-elf-gcc tests/$(file).c -o out/$(file).out -Os -nostartfiles
 win:
-	riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 tests/$(file).c -o out/$(file).out -Os -nostartfiles
+	riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 tests/$(file).c -o out/$(file).out -Os -nostartfiles -nodefaultlibs -nostdlib
 clean:
 	rm out/*.out
--- test.sh
+++ test.sh
@@ -1,16 +1,14 @@
-sudo make clean
-
-make $1 file=blink_single
-make $1 file=blink
-
-make $1 file=button
-
-make $1 file=switch_simple
-make $1 file=switch
-make $1 file=switches_simple
-make $1 file=switches 
-
-make $1 file=uart_print
-make $1 file=uart_usb_simple 
-
+sudo make clean 
+ 
+make $1 file=blink_single 
+make $1 file=blink 
+ 
+make $1 file=button 
+ 
+make $1 file=switch_simple 
+make $1 file=switch 
+make $1 file=switches_simple 
+ 
+make $1 file=uart_usb_simple  
+ 
 make $1 file=vga_simple
