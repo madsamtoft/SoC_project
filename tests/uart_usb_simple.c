0150abe152c0d7c4e3c994edc06a420e6fd11f28
madsamtoft <mads.amtoft@hotmail.com>
2025-06-09 18:07:51 +0200

    test first char

--- tests/uart_usb_simple.c
+++ tests/uart_usb_simple.c
@@ -2,11 +2,12 @@
 
 int main(void) {
     volatile int ps2_data;
-    volatile char key;
+    //volatile char key;
+    setUart('-');
     while(1) {
         ps2_data = readPs2();
         //key = readKey(ps2_data);
         //setUart(key);
-        setUart(ps2_data + '0');
+        setUart(ps2_data);
     }
 }
