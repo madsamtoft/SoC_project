85415f621dde2db7ea2d41a826c89dafa5303434
Dejan <deja0079@outlook.dk>
2025-06-10 13:10:06 +0200

    Started working on Pong

--- programs/pong.c
+++ programs/pong.c
@@ -1,53 +1,57 @@
-// #include <stdio.h>
+#include "../tests/wildvga.h"
+#include "../tests/wildio.h"
 
-// VGA base address: 0xf010_0000
-// VGA address decomposition: 0b00000000_00000000_0yyyyyyy_xxxxxxxx
-// VGA data: 0b00000000_00000000_00000000_00000000_00rrbbgg
-#define VGA_BASE_ADDR 0xf0100000
-int volatile * const vga = (volatile int *) VGA_BASE_ADDR;
+#define BALL_RADIUS 3
 
 struct pongBall {
     int x;
     int y;
+    int vx;
+    int vy;
 };
 typedef struct pongBall Ball;
 
-void printPixel(int x, int y);
-void printBall(Ball *ball);
+void drawBall(Ball *ball);
+void updateBall(Ball* ball);
 
 int main() {
-    Ball ball = {0, 0};
+    Ball ball = {0, 0, 1, 1};
     Ball *ballPointer = &ball;
 
-    printBall(ballPointer);
-
-    // printf("x = %d\n", ball.x);
-    // printf("y = %d", ball.y);
+    while(1) {
+        drawBall(ballPointer);
+        updateBall(ballPointer);
+        wait(1666666);
+    }
 
     return 0;
 }
 
-
-
-
-
-void printPixel(int x, int y) {
-
-}
-
-void printBall(Ball *ball) {
+void drawBall(Ball* ball) {
     int x = ball->x;
     int y = ball->y;
 
-    printPixel(x-1, y-1);
-    printPixel(x,   y-1);
-    printPixel(x+1, y-1);
+    drawCircle(x, y, BALL_RADIUS, WHITE, 0);
+}
 
-    printPixel(x-1, y);
-    printPixel(x,   y);
-    printPixel(x+1, y);
-    
-    printPixel(x-1, y+1);
-    printPixel(x,   y+1);
-    printPixel(x+1, y+1);
+void updateBall(Ball* ball) {
+    int oldX = ball->x;
+    int oldY = ball->y;
+    int vx = ball->vx;
+    int vy = ball->vy;
+
+    if(oldX+BALL_RADIUS*2 >= VGA_X_LIM-1 || oldX <= 0) {
+        vx *= -1;
+    }
+    if(oldY+BALL_RADIUS*2 >= VGA_Y_LIM-1 || oldY <= 0) {
+        vy *= -1;
+    }
+
+    int newX = oldX + vx;
+    int newY = oldY + vy;
+
+    ball->vx = vx;
+    ball->vy = vy;
+    ball->x = newX;
+    ball->y = newY;
 }
