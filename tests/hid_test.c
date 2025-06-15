a30c9791e63d6ce6c65637b64f807c8181e20869
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-06-15 23:11:18 +0200

    Updated scancodes WASD

---
+++ tests/hid_test.c
@@ -0,0 +1,35 @@
+#include "wildio.h"
+
+int main(void) {
+
+    volatile char ps2_data;
+
+    setUart('-');  // Initial UART debug output
+
+    while (1) {
+        ps2_data = readPs2();         // Read from PS/2 (MMIO at 0xf0040000)
+
+        switch (ps2_data & 0xFF) {    // Always mask to ensure 8-bit comparison
+            case W:
+                setUart('1');
+                break;
+
+            case A:
+                setUart('2');
+                break;
+
+            case S:                // Another example code
+                setUart('3');
+                break;
+
+            case D:                // Another example code
+                setUart('4');
+                break;
+
+
+            default:
+                setUart('0');
+                break;
+        }
+    }
+}
