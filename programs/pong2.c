9de4aebe7e42ab00e5295f250779ce2d091f0f3a
madsamtoft <mads.amtoft@hotmail.com>
2025-06-16 18:33:21 +0200

    effort to fix

--- programs/pong2.c
+++ programs/pong2.c
@@ -36,7 +36,10 @@ int main() {
         // Wait for timer to run out
         waitTimer();
     }
+    
+    startTimer(1000);
     setLeds(0xFFFF);
+    waitTimer();
     return 0;
 }
 
@@ -52,13 +55,23 @@ void updateBall(Ball* ball) {
     int vy = ball->vy;
 
     // Bounce off left/right walls
-    if (x <= 0 || x + (BALL_RADIUS*2) >= VGA_X_LIM) {
+    if (x <= 0) {
         vx *= -1;
+        x = 0;
+    } else if (x + (BALL_RADIUS*2) >= VGA_X_LIM) {
+        vx *= -1;
+        x = VGA_X_LIM - 2*BALL_RADIUS;
     }
+
     // Bounce off top/bottom walls
-    if (y <= 0 || y + (BALL_RADIUS*2) >= VGA_Y_LIM) {
+    if (y <= 0) {
         vy *= -1;
+        y = 0;
+    } else if (y + (BALL_RADIUS*2) >= VGA_Y_LIM) {
+        vy *= -1;
+        y = VGA_Y_LIM - 2*BALL_RADIUS;
     }
+    
 
     ball->x += vx;
     ball->y += vy;
